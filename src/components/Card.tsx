db03fec5fde6fb88873b58bf5858c3b54fc3c0e9
Walmonn Eduardo <130590544+WalmonnEduardo@users.noreply.github.com>
2025-03-07 22:34:39 -0300

    Add files via upload

---
+++ src/components/Card.tsx
@@ -0,0 +1,48 @@
+interface PropsCard{
+    temp:number | null;
+    veloc:number | null;
+    sensacao:number | null;
+    nome:string;
+    img:string;
+}
+export default function Card(props:PropsCard){
+    return(
+        <div className="flex flex-col cardprinc w-8/12 h-1/4 overflow-hidden">
+            <div className="h-2/12 w-full flex items-center justify-center camp-t">
+                <h1 className="font-bold text-white md:text-4xl sm:text-xl">{props.nome}</h1>
+            </div>
+            <div className="h-full w-full md:flex md:flex-row sm:flex sm:flex-col justify-between items-center">
+                
+                <div className="h-full float-left overflow-hidden">
+                    <img className="h-full w-80" src={props.img} alt={props.nome} />
+                </div>
+                <div className="h-11/12 w-full ">
+                    <table className="tab">
+                        <thead>
+                            <tr>
+                                <td className="font-bold text-black md:text-3xl md:p-1 text-center sm:text-xl sm:p-1">Tempo atual</td>
+                                <td className="font-bold text-black md:text-3xl md:p-1 text-center sm:text-xl sm:p-1">Dados</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            
+                            <tr>
+                                <td className="text-color-gray-700  md:text-2xl md:p-1 sm:p-1">Temperatura</td>
+                                <td className="text-color-gray-700  md:text-2xl md:p-1 sm:p-1">{props.temp}°C</td>
+                            </tr>
+                            <tr>
+                                <td className="text-color-gray-700  md:text-2xl md:p-1 sm:p-1">Sensação Térmica</td>
+                                <td className="text-color-gray-700  md:text-2xl md:p-1 sm:p-1">{props.sensacao}°C</td>
+                            </tr>
+                            <tr>
+                                <td className="text-color-gray-700  md:text-2xl md:p-1 sm:p-1">Velocidade do vento</td>
+                                <td className="text-color-gray-700  md:text-2xl md:p-1 sm:p-1">{props.veloc}km/h</td>
+                            </tr>
+                            
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/pages/Cidade/Cidade.tsx
+++ src/pages/Cidade/Cidade.tsx
@@ -1,29 +1,54 @@
 import { useState,useEffect } from "react";
 import Topo from "@/components/Topo";
 import Footer from "@/components/Footer";
-import CardPrincipal from "@/components/CardPrincipal";
+import Card from "@/components/Card";
 
 export default function Cidade() {
-  let cidade: string = "Foz_do_Iguaçu";
-  const [temp, setTemp] = useState<number | null>(null);
-  const [veloc, setVeloc] = useState<number | null>(null);
-  const [sensacao, setSensacao] = useState<number | null>(null);  
+  const [tempn, setTempn] = useState<number | null>(null);
+  const [velocn, setVelocn] = useState<number | null>(null);
+  const [sensacaon, setSensacaon] = useState<number | null>(null);  
+  const [temps, setTemps] = useState<number | null>(null);
+  const [velocs, setVelocs] = useState<number | null>(null);
+  const [sensacaos, setSensacaos] = useState<number | null>(null);  
+  const [tempr, setTempr] = useState<number | null>(null);
+  const [velocr, setVelocr] = useState<number | null>(null);
+  const [sensacaor, setSensacaor] = useState<number | null>(null);  
   useEffect(() => {
-    let cidade: string = "Foz_do_Iguaçu";
-    fetch(`https://api.weatherapi.com/v1/current.json?key=ffb45b876e37459fbe1145531250503&q=${cidade}&aqi=no`)
+    let cidade1: string = "Natal";
+    let cidade3: string = "São_Paulo";
+    let cidade2: string = "Rio_de_Janeiro";
+    fetch(`https://api.weatherapi.com/v1/current.json?key=ffb45b876e37459fbe1145531250503&q=${cidade1}&aqi=no`)
       .then(response => response.json())
       .then(data => {
-        setTemp(data.current.temp_c?data.current.temp_c:0);
-        setVeloc(data.current.wind_kph?data.current.wind_kph:0);
-        setSensacao(data.current.feelslike_c?data.current.feelslike_c:0);
+        setTempn(data.current.temp_c?data.current.temp_c:0);
+        setVelocn(data.current.wind_kph?data.current.wind_kph:0);
+        setSensacaon(data.current.feelslike_c?data.current.feelslike_c:0);
+      })
+      .catch(error => console.error("Erro ao buscar os dados:", error));
+      fetch(`https://api.weatherapi.com/v1/current.json?key=ffb45b876e37459fbe1145531250503&q=${cidade2}&aqi=no`)
+      .then(response => response.json())
+      .then(data => {
+        setTemps(data.current.temp_c?data.current.temp_c:0);
+        setVelocs(data.current.wind_kph?data.current.wind_kph:0);
+        setSensacaos(data.current.feelslike_c?data.current.feelslike_c:0);
+      })
+      .catch(error => console.error("Erro ao buscar os dados:", error));
+      fetch(`https://api.weatherapi.com/v1/current.json?key=ffb45b876e37459fbe1145531250503&q=${cidade3}&aqi=no`)
+      .then(response => response.json())
+      .then(data => {
+        setTempr(data.current.temp_c?data.current.temp_c:0);
+        setVelocr(data.current.wind_kph?data.current.wind_kph:0);
+        setSensacaor(data.current.feelslike_c?data.current.feelslike_c:0);
       })
       .catch(error => console.error("Erro ao buscar os dados:", error));
   }, []);
   return (
     <div className="h-full w-full flex flex-col min-h-screen">
       <Topo></Topo>
-      <main className="flex items-center justify-center flex-grow h-10 w-full main-princ">
-        <CardPrincipal temp={temp} veloc={veloc} sensacao={sensacao}></CardPrincipal>
+      <main className="flex items-center justify-evenly flex-col flex-grow h-10 w-full main-princ">
+        <Card temp={tempn} veloc={velocn} sensacao={sensacaon} nome="Natal-RN" img="/img/nat.jpeg"></Card>
+        <Card temp={temps} veloc={velocs} sensacao={sensacaos} nome="São Paulo-SP" img="/img/sao.jpeg"></Card>
+        <Card temp={tempr} veloc={velocr} sensacao={sensacaor} nome="Rio de Janeiro-RJ" img="/img/rio.jpeg"></Card>
       </main>
       <Footer></Footer>
     </div>
